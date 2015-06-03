e6d8d14940a574e888d825655489dbd82b069aab
Timothy Moose <wtm@tractablelabs.com>
2015-06-03 12:32:24 -0500

    Remove import for direct source inclusion

--- Result/Result.swift
+++ Result/Result.swift
@@ -1,7 +1,5 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-import Box
-
 /// An enum representing either a failure with an explanatory error, or a success with a result value.
 public enum Result<T, Error>: Printable, DebugPrintable {
 	case Success(Box<T>)
